54c3a6fe52edca0bb07b67896f54fbc234da6115
BaavanaBandarupalli <133509518+BaavanaBandarupalli@users.noreply.github.com>
2025-04-18 11:32:05 +0530

    Add files via upload

---
+++ HospitalManagementSystem/HospitalManagementSystem/src/com/hospital/exception/PatientNumberNotFoundException.java
@@ -0,0 +1,14 @@
+package com.hospital.exception;
+
+public class PatientNumberNotFoundException extends Exception{
+	
+	public PatientNumberNotFoundException() {
+		super();
+	}
+
+	public PatientNumberNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
